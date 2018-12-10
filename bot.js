e991efd9eaa4b3c1543d7d15f813def1732db319
Matur <42393036+Bidoofnationwillprevail@users.noreply.github.com>
2018-12-10 18:12:11 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,13 +3,14 @@ const discord = require(`discord.js`),
       test = require(`https://github.com/Bidoofnationwillprevail/Bot/blob/master/Json.json`),
       client = new discord.Client({disableEveryone: true});
 client.login(process.env.BOT_TOKEN);
-client.on('error', console.error);
 client.on(`ready`, () => {
   client.user.setPresence({status: "online", game: {name: "World Domination", type: "PLAYING" }});
 });
 client.on(`message`, (message) => {
-  if (message.author.bot || message.channel.type != `text`) {return;}
-  if (!test.hasOwnProperty(message.author.name)) {test[message.author.name] = 0;}
-  test[message.author.name] += 1;
-  fs.writeFile(`./Test.json`, JSON.stringify(test, null, 4), (error) => {if (error) {throw error;}});
-});
+  try {
+        if (message.author.bot || message.channel.type != `text`) {return;}
+        if (!test.hasOwnProperty(message.author.name)) {test[message.author.name] = 0;}
+        test[message.author.name] += 1;
+        fs.writeFile(`https://github.com/Bidoofnationwillprevail/Bot/blob/master/Json.json`, JSON.stringify(test, null, 4), (error) => {if (error) {throw error;}});
+  } catch (error) {message.guild.owner.send(`ERROR`);}
+  });
